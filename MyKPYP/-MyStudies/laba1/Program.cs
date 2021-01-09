bebe5421958c21c4feaccd400f4beede82d4b6aa
vladura4ok <68918856+vladura4ok@users.noreply.github.com>
2021-01-09 22:53:09 +0300

    Add files via upload

---
+++ MyKPYP/-MyStudies/laba1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a,b;
+            b = 2;
+            a = b / (3 * 4 * 5);
+            Console.WriteLine(a);
+            Console.WriteLine((2-37*5.34)/2);
+            Console.WriteLine((7-2)/(3-5));
+            Console.WriteLine("*       *  *           *      ****   ");
+            Console.WriteLine(" *     *   *          * *     *   ** ");
+            Console.WriteLine("  *   *    *         *   *    *    **");
+            Console.WriteLine("   * *     *        *******   *   ** ");
+            Console.WriteLine("    *      ******  *       *  ****   ");
+
+        }
+    }
+}
