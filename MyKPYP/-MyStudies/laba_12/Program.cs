3ccad2d95ec6ec1884f04f07f80d669c3deb09d9
vladura4ok <68918856+vladura4ok@users.noreply.github.com>
2021-01-09 22:51:45 +0300

    Add files via upload

---
+++ MyKPYP/-MyStudies/laba_12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LabaKPu9IP_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] mass = new int[5];
+
+                Console.WriteLine("Введите 5 чисел: ");
+                for (int i = 0; i < 5; i++)
+                {
+                    mass[i] = int.Parse(Console.ReadLine());
+                }
+
+                int sum = 0;
+                int count = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+
+                    if (mass[i] != 0)
+                    {
+                        sum += mass[i];
+                        count++;
+                    }
+                }
+
+                Console.WriteLine("Среднее арифметическое: " + sum / count);
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
