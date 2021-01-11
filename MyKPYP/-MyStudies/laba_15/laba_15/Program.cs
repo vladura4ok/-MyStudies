c41505701880641f33b1d93121d22ca9f1e95d6a
vladurak <68918856+vladurak@users.noreply.github.com>
2021-01-11 14:17:37 +0300

    Add files via upload

---
+++ MyKPYP/-MyStudies/laba_15/laba_15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace _15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------");
+            string path = @"/Users/VlaD/Desktop/avto.txt";
+            string[] mass = File.ReadAllLines(path);
+            Console.Write("Ввдете номер цеха: ");
+            string tex = Console.ReadLine();
+            Console.WriteLine();
+            List<int> result  = new List<int>();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                string[] newMass = mass[i].Split(" ");
+                result.Add(Convert.ToInt32(newMass[1]));
+            }
+            result.Sort();
+            result.Reverse();
+            
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int j = 0; j < mass.Length; j++)
+                {
+                    string[] newMass = mass[j].Split(" ");
+                    if (result[i] == int.Parse(newMass[1]) && tex == newMass[2])
+                        Console.WriteLine(mass[j]);
+                }
+            }
+
+        }
+    }
+}
