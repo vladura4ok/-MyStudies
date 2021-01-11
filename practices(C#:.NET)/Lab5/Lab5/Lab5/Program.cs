e7e9138ccc9fff2ab9cec3b33e37fd31569e3097
vladurak <68918856+vladurak@users.noreply.github.com>
2021-01-11 17:12:09 +0300

    Add files via upload

---
+++ practices(C#:.NET)/Lab5/Lab5/Lab5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a, b, d;
+            char c;
+
+            Console.Write("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите действие (+, -, *, /): ");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            switch (c)
+            {
+                case '+': 
+                    d = a + b; 
+                    Console.WriteLine(a + " + " + b + " = " + d); 
+                    break;
+                case '-': 
+                    d = a - b; 
+                    Console.WriteLine(a + " - " + b + " = " + d); 
+                    break;
+                case '*': 
+                    d = a * b; 
+                    Console.WriteLine(a + " * " + b + " = " + d); 
+                    break;
+                case '/': 
+                    d = a / b; 
+                    Console.WriteLine(a + " / " + b + " = " + d); 
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
