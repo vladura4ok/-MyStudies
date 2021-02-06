d0f3b93c240d145f236de0d4473a5da3fa7a6747
vladurak <68918856+vladurak@users.noreply.github.com>
2021-02-06 10:06:04 +0300

    Add files via upload

---
+++ MyKPYP/-MyStudies/laba_16/ClassLibraryProjects/StringLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    public static class StringLibrary
+    {
+        public static int StartsWithUpper(int first, int second)
+        {
+            int result = 0;
+            for (int i = 0; i < first; i++)
+                result += second;
+            return result;
+        }
+    }
+}
